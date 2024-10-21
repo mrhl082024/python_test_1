5312dbb99a882454b0fe5d995f3ecd2f4d30b5f1
mrhl082024 <mhrl082024@gmail.com>
2024-10-21 13:52:05 +0200

    first time coding with python

---
+++ main.py
@@ -0,0 +1,13 @@
+x = 5
+y = 11
+z = 8
+print (x + y + z)
+
+my_name = "Lars"
+print(my_name)
+
+my_age = (x + y + z)
+print (my_age)
+
+my_hobbies = ["friends", "music", "videogames", "cars"]
+print (my_hobbies)
