0436f8460837499102ed55f8a59a68e8da99994a
mrhl082024 <mhrl082024@gmail.com>
2024-10-21 14:43:34 +0200

    lagde en liten liste, lagt inn kommentarer på ting

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+
+#printer ut noen tall som er lagt sammen
 x = 5
 y = 11
 z = 8
 print (x + y + z)
 
+#lagde en verdi som inneholder et navn
 my_name = "Lars"
 print(my_name)
 
+#legger sammen x, y, z til en verdi
 my_age = (x + y + z)
 print (my_age)
 
-my_hobbies = ["friends", "music", "videogames", "cars"]
-print (my_hobbies)
+
+#lagd en list som printer ut "banana"
+my_list = ["apple", "banana", "orange"]
+print (my_list[1])
