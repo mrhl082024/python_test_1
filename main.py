ddcf1de2d26cefa717c38789f1c162760bf0b6c8
mrhl082024 <mhrl082024@gmail.com>
2024-10-22 09:19:13 +0200

    laget en ny liste funksjon og noen kommentarer

--- main.py
+++ main.py
@@ -19,3 +19,12 @@ my_list = ["apple", "banana", "orange"]
 print (my_list[1])
 #printer ut hvor mange "ting" som er i listen
 print (len(my_list))
+#ser om "apple" er i listen, og gir en melding hvis den er.
+if "apple" in my_list:
+    print ("apple in this list")
+
+
+#byttet ut "volvo" til "mercedez"
+new_list = ["bmw", "volvo", "audi"]
+new_list[1] = "mercedez"
+print (new_list)
